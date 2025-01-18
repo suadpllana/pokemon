3a8de6fbcb1565eb7ef8eed6a5eefafc0b18614b
oltapllana <oltapllana@gmail.com>
2025-01-18 01:50:20 +0100

    first commit

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -24,6 +24,7 @@ function Pokemon() {
       setNextUrl(data.next);
       setPrevUrl(data.previous);
       getPokemon(data.results);
+
       setLoading(false);
     }
     getPokemonData();
@@ -37,6 +38,7 @@ function Pokemon() {
       })
     );
     setPokemonData(pokemonArray);
+    setFilteredPokemon([pokemonArray[0]]);
   }
 
   function getPokemonById(id) {
@@ -49,14 +51,30 @@ function Pokemon() {
         <>
           <div className="pokemon-display">
             {pokemonData.map((pokemon) => (
-              <div key={pokemon.id} onClick={() => getPokemonById(pokemon.id)} className="pokemon">
+              <div
+                key={pokemon.id}
+                onClick={() => getPokemonById(pokemon.id)}
+                className="pokemon"
+              >
                 <p>{pokemon.id}</p>
                 <img src={pokemon.sprites.front_default} alt={pokemon.name} />
                 <p>{pokemon.name}</p>
               </div>
             ))}
-            <button className="prev" onClick={() => setUrl(prevUrl)} disabled={!prevUrl}>Prev</button>
-            <button className="next" onClick={() => setUrl(nextUrl)} disabled={!nextUrl}>Next</button>
+            <button
+              className="prev"
+              onClick={() => setUrl(prevUrl)}
+              disabled={!prevUrl}
+            >
+              Prev
+            </button>
+            <button
+              className="next"
+              onClick={() => setUrl(nextUrl)}
+              disabled={!nextUrl}
+            >
+              Next
+            </button>
           </div>
           <div className="filtered-pokemon-container">
             {filteredPokemon.map((pokemon) => (
@@ -65,24 +83,69 @@ function Pokemon() {
                 <img src={pokemon.sprites.front_default} alt={pokemon.name} />
                 <div className="button-ability">
                   {pokemon.abilities.map((ability) => (
-                    <button key={ability.ability.name}>{ability.ability.name}</button>
+                    <button key={ability.ability.name}>
+                      {ability.ability.name}
+                    </button>
                   ))}
                 </div>
-                {pokemon.stats.map((stat, index) => (
-                  <p key={index}>
-                    {index === 0 && <CiHeart />} {index === 1 && <LuSwords />} {index === 2 && <FaShieldAlt />} {index === 3 && <GiSwordsEmblem />} {index === 4 && <IoShield />} {index === 5 && <FaBoltLightning />} 
-                    {stat.stat.name}: {stat.base_stat}
-                    <div style={{ width: "200px", height: "10px", background: "#ddd", position: "relative", marginLeft: "80px" }}>
-                      <div style={{ width: `${stat.base_stat}%`, height: "100%", background: "blue", transition: "width 0.3s ease" }}></div>
-                    </div>
-                  </p>
-                ))}
-                <p><GiStrongMan /> Power: {pokemon.base_experience}</p>
-                <p><MdBloodtype /> Types: {pokemon.types.map((t) => t.type.name).join(", ")}</p>
+                {pokemon.stats.map((stat, index) => {
+                 const statColor =
+                 stat.stat.name === "hp"
+                   ? "#78C850" 
+                   : stat.stat.name === "attack"
+                   ? "#F08030" 
+                   : stat.stat.name === "defense"
+                   ? "#F85888" 
+                   : stat.stat.name === "speed"
+                   ? "#F8D030" 
+                   : stat.stat.name === "special-attack"
+                   ? "#6890F0" 
+                   : stat.stat.name === "special-defense"
+                   ? "#FF5959" 
+                   : "#A8A878"; 
+               
+                  return (
+                    <p key={index}>
+                      {index === 0 && <CiHeart />}
+                      {index === 1 && <LuSwords />}
+                      {index === 2 && <FaShieldAlt />}
+                      {index === 3 && <GiSwordsEmblem />}
+                      {index === 4 && <IoShield />}
+                      {index === 5 && <FaBoltLightning />}
+                      {stat.stat.name}: {stat.base_stat}
+                      <div
+                        style={{
+                          width: "200px",
+                          height: "10px",
+                          background: "#ddd",
+                          position: "relative",
+                          marginLeft: "80px",
+                        }}
+                      >
+                        <div
+                          style={{
+                            width: `${stat.base_stat}%`,
+                            height: "100%",
+                            background: statColor,
+                            transition: "width 0.3s ease",
+                          }}
+                        ></div>
+                      </div>
+                    </p>
+                  );
+                })}
+
+                <p>
+                  <GiStrongMan /> Power: {pokemon.base_experience}
+                </p>
+                <p>
+                  <MdBloodtype /> Types:{" "}
+                  {pokemon.types.map((t) => t.type.name).join(", ")}
+                </p>
                 <p>Moves:</p>
                 <ul>
                   {pokemon.moves.slice(0, 10).map((move) => (
-                    <p  key={move.move.name}>{move.move.name}</p>
+                    <p key={move.move.name}>{move.move.name}</p>
                   ))}
                 </ul>
               </div>
